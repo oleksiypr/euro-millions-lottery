d2c09ac5f73db97d9ee5bc2b50ca565c2e24c6e0
oleksii.prosianko <oleksii.prosianko@globallogic.com>
2018-05-05 22:41:13 +0300

    eSlr: assessment: EuroLotteryTest: improvement

--- src/test/scala/op/assessment/eslr/EuroLotteryTest.scala
+++ src/test/scala/op/assessment/eslr/EuroLotteryTest.scala
@@ -7,99 +7,63 @@ class EuroLotteryTest extends FunSuite {
 
   test("data structure") {
     Ticket("1, 2, 3, 4, 5 : 1, 2") match {
-      case Some(NormalTicket(ns, ss)) =>
-        assert(ns === Set(1, 2, 3, 4, 5))
-        assert(ss === Set(1, 2))
-      case x => assert(x === None)
+      case Some(ticket) =>
+        assert(ticket == NormalTicket(Set(1, 2, 3, 4, 5), Set(1, 2)))
+      case None => fail("Some ticket expected but actual none")
     }
 
     Ticket("1, 2, 3, 4, 5, 6 : 1, 2") match {
-      case Some(SystemTicket(ns, ss)) =>
-        assert(ns === Set(1, 2, 3, 4, 5, 6))
-        assert(ss === Set(1, 2))
-      case x => assert(x === None)
+      case Some(ticket) =>
+        assert(ticket == SystemTicket(Set(1, 2, 3, 4, 5, 6), Set(1, 2)))
+      case None => fail("Some ticket expected but actual none")
     }
 
     Ticket("1, 2, 3, 4, 5 : 1, 2, 3") match {
-      case Some(SystemTicket(ns, ss)) =>
-        assert(ns === Set(1, 2, 3, 4, 5))
-        assert(ss === Set(1, 2, 3))
-      case x => assert(x === None)
+      case Some(ticket) =>
+        assert(ticket == SystemTicket(Set(1, 2, 3, 4, 5), Set(1, 2, 3)))
+      case None => fail("Some ticket expected but actual none")
     }
 
     Ticket("1, 2, 3, 4, 5, 6, 7, 8, 9, 10 : 1, 2") match {
-      case Some(SystemTicket(ns, ss)) =>
-        assert(ns === Set(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
-        assert(ss === Set(1, 2))
-      case x => assert(x === None)
+      case Some(ticket) =>
+        assert(ticket == SystemTicket(
+          Set(1, 2, 3, 4, 5, 6, 7, 8, 9, 10), Set(1, 2)))
+      case None => fail("Some ticket expected but actual none")
     }
 
     Ticket("1, 2, 3, 4, 5 : 1, 2, 3, 4, 5") match {
-      case Some(SystemTicket(ns, ss)) =>
-        assert(ns === Set(1, 2, 3, 4, 5))
-        assert(ss === Set(1, 2, 3, 4, 5))
-      case x => assert(x === None)
+      case Some(ticket) =>
+        assert(ticket == SystemTicket(
+          Set(1, 2, 3, 4, 5), Set(1, 2, 3, 4, 5)))
+      case None => fail("Some ticket expected but actual none")
     }
 
     Ticket("1, 2, 3, 4, 5, 6 : 1, 2, 3") match {
-      case Some(SystemTicket(ns, ss)) =>
-        assert(ns === Set(1, 2, 3, 4, 5, 6))
-        assert(ss === Set(1, 2, 3))
-      case x => assert(x === None)
+      case Some(ticket) =>
+        assert(ticket == SystemTicket(
+          Set(1, 2, 3, 4, 5, 6), Set(1, 2, 3)))
+      case None => fail("Some ticket expected but actual none")
     }
   }
 
   test("data structure: empty numbers") {
-    Ticket(": 1, 2") match {
-      case None => succeed
-      case x => assert(x === None)
-    }
+    assert(Ticket(": 1, 2") === None)
   }
 
   test("data structure: empty stars") {
-    Ticket("1, 2, 3, 4, 5 :") match {
-      case None => succeed
-      case x => assert(x === None)
-    }
-    Ticket("1, 2, 3, 4, 5") match {
-      case None => succeed
-      case x => assert(x === None)
-    }
+    assert(Ticket("1, 2, 3, 4, 5 :") === None)
+    assert(Ticket("1, 2, 3, 4, 5") === None)
   }
 
   test("data structure: incorrect input") {
-    Ticket("") match {
-      case None => succeed
-      case x => assert(x === None)
-    }
-    Ticket("1, 2, 3, as, 5 : 1, 2") match {
-      case None => succeed
-      case x => assert(x === None)
-    }
-    Ticket("sdfdsfsd") match {
-      case None => succeed
-      case x => assert(x === None)
-    }
-    Ticket("1, 2, 3, 51, 5 : 1, 2") match {
-      case None => succeed
-      case x => assert(x === None)
-    }
-    Ticket("1, 2, 3, 4, 5 : 1, 12") match {
-      case None => succeed
-      case x => assert(x === None)
-    }
-    Ticket("1, 0, 3, 4, 5 : 1, 6") match {
-      case None => succeed
-      case x => assert(x === None)
-    }
-    Ticket("-1, 2, 3, 4, 5 : 1, 6") match {
-      case None => succeed
-      case x => assert(x === None)
-    }
-    Ticket("1, 2, 3, 4, 5 : 0, 11") match {
-      case None => succeed
-      case x => assert(x === None)
-    }
+    assert(Ticket("") === None)
+    assert(Ticket("1, 2, 3, as, 5 : 1, 2") === None)
+    assert(Ticket("sdfdsfsd") === None)
+    assert(Ticket("1, 2, 3, 51, 5 : 1, 2") === None)
+    assert(Ticket("1, 2, 3, 4, 5 : 1, 12") === None)
+    assert(Ticket("1, 0, 3, 4, 5 : 1, 6") === None)
+    assert(Ticket("-1, 2, 3, 4, 5 : 1, 6") === None)
+    assert(Ticket("1, 2, 3, 4, 5 : 0, 11") === None)
   }
 
   test("all normal tickets") {
