731c98286c8062c325f94ecb550a00ac23cf6f10
oleksii.prosianko <oleksii.prosianko@globallogic.com>
2018-05-06 11:26:11 +0300

    eSlr: assessment: Solver implemented

--- src/main/scala/op/assessment/eslr/Main.scala
+++ src/main/scala/op/assessment/eslr/Main.scala
@@ -1,17 +1,35 @@
 package op.assessment.eslr
 
+import op.assessment.eslr.EuroLottery.{NormalTicket, Ticket}
+
 object Main extends App {
 
-  def combinations(set: Set[Int], m: Int): Set[Set[Int]] = {
-    if (m == 0) Set(Set.empty[Int]) else
-    if (set.isEmpty) Set(Set.empty[Int]) else
-    if (m >= set.size) Set(set) else {
-      for {
-        e <- set
-        comb <- combinations(set - e, m - 1)
-      } yield comb + e
-    }
-  }
+  type WinningClass = NormalTicket => Int
 
-  println(combinations(Set(1, 2, 3, 4, 5), 3))
+  val inputs = List(
+    "1, 2, 3, 4, 5 : 1, 2",
+    "3, 4, 5, 6, 7 : 1, 2, 3",
+    "3, 4, 5, 6, 7 : 2, 3",
+    "6, 7, 8, 9, 10 : 3, 4"
+  )
+
+  val solver = new Solver(
+    winNumbers = Set(1, 2, 3, 4, 5),
+    winStars = Set(1, 2)
+  )
+
+  val winningClass: WinningClass = nt => solver(nt.numbers, nt.stars)
+
+  val result = inputs
+    .flatMap(Ticket(_))
+    .flatMap(_.normalTickets)
+    .groupBy(winningClass)
+    .toArray.sortBy(_._1)
+    .flatMap {
+      case (0, _) => None
+      case (k, v) => Some(s"Winning class $k: ${v.size}")
+    }
+
+  result foreach println
+
 }
---
+++ src/main/scala/op/assessment/eslr/Solver.scala
@@ -0,0 +1,42 @@
+package op.assessment.eslr
+
+/**
+  * Checks number and stars combination against winning combinations in order
+  * to resolve winning class.
+  *
+  * @param winNumbers winning numbers as a set of 5 distinct integers
+  *                   from 1 to 50 each
+  * @param winStars winning stars as a set of 2 distinct integers 1 to 11 each
+  */
+class Solver(winNumbers: Set[Int], winStars: Set[Int]) {
+
+  /**
+    * Resolve winning class as integer number from 1 to 13.
+    * If a ticket does not correspond to any wining class result will be 0.
+    *
+    * @param numbers set of 5 distinct integers from 1 to 50 each
+    * @param stars set of 2 distinct integers 1 to 11 each
+    * @return winning class as integer number
+    */
+  def apply(numbers: Set[Int], stars: Set[Int]): Int = {
+    val ns = (winNumbers intersect numbers).size
+    val ss = (winStars intersect stars).size
+
+    (ns, ss) match {
+      case (5, 2) =>  1
+      case (5, 1) =>  2
+      case (5, 0) =>  3
+      case (4, 2) =>  4
+      case (4, 1) =>  5
+      case (4, 0) =>  6
+      case (3, 2) =>  7
+      case (2, 2) =>  8
+      case (3, 1) =>  9
+      case (3, 0) => 10
+      case (1, 2) => 11
+      case (2, 1) => 12
+      case (2, 0) => 13
+      case (_, _) =>  0
+    }
+  }
+}
